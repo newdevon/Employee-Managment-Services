bd76d18e0e9f79a6cbac32dca18091811c588c4b
newdevon <stvchan999@gmail.com>
2022-08-10 17:03:25 -0400

    EMS-3: connecting components, creating employees

--- app.py
+++ app.py
@@ -31,9 +31,12 @@ class Department:
         # return self.empl_query
 
     def list_departments():
-        print("I am here!")
-        for dept in Department.dept_list:
-            print(f"{dept.name}")
+        dept_names = [dept.name for dept in Department.dept_list]
+        print(dept_names)
+    
+    def find_manager():
+        pass   
+        
 
 class Employee:
     emplist = []
@@ -71,13 +74,13 @@ class Employee:
     
         print(f"{name} has been removed from All Employees List")
         
-class SalaryException (Exception): pass
-try:
-    salary = int(input("Please enter your yearly salary"))
-    if salary < 40000:
-        raise SalaryException("This salary is too low")
-except SalaryException as e:
-    print(e)
+# class SalaryException (Exception): pass
+# try:
+#     salary = int(input("Please enter your yearly salary"))
+#     if salary < 40000:
+#         raise SalaryException("This salary is too low")
+# except SalaryException as e:
+#     print(e)
     
 
 employee1 = Employee('kenny', 'yang', 50000, 'Finance', False)
@@ -92,10 +95,10 @@ employee9 = Employee('Smain', 'Benloukil', 49000, 'Sales', False)
 
 employee1.gen_randomid()
 employee2.gen_randomid()
-print(employee1.empid, employee2.empid)
+# print(employee1.empid, employee2.empid)
 
-finance = Department("finance")
-sales = Department("sales")
+finance = Department("Finance")
+sales = Department("Sales")
 
 finance.add_employee(employee1)
 finance.add_employee(employee2)
@@ -103,7 +106,7 @@ finance.add_employee(employee3)
 finance.add_employee(employee6)
 finance.add_employee(employee7)
 
-finance.list_employee()
+# finance.list_employee()
 # for employee in Employee.emplist:
 #     print(f"{employee.first} {employee.last}")
 
@@ -115,29 +118,50 @@ finance.list_employee()
 start = int(input("1) View Departments\n2) Add Departments\nChooose (1/2): "))
 while True:
     if start == 1:
-        print(f"Here are the departments:\n")
-        Department.list_departments()
-        select_dept = input("1) Finance\n2) Sales\n")
-        if select_dept == 1:
-            viewsales = input("SALES DEPARTMENT\n1) View employees\n3) Add employees\n4) Return to previous menu.")
-            if viewsales == 1:
-                print("Here are all the Employees {Employee.emplist}")
-            elif viewsales == 2:
-                pass
-            elif viewsales == 3:
-                pass
+        print(f"Here are the departments:")
+        #Department.list_departments()
+
+        for i in range(len(Department.dept_list)):
+            print(f"{i + 1}) {Department.dept_list[i].name}")
+
+        select_dept = int(input("Enter digit for which department: "))
+        viewCurrent = int(input(f"{Department.dept_list[select_dept-1].name} Department\n1) View employees\n2) Add employees\n3) Return to previous menu."))
+        if viewCurrent == 1:
+            print("Here are all the Employees {Employee.emplist}")
+        elif viewCurrent == 2:
+            while True:
+                class SalaryException (Exception): pass
+                try:
+                    emp_first = input("Enter Employee First Name: ")
+                    emp_last= input("Enter Employee Last Name: ")
+                    emp_salary = int(input("Please enter employee yearly salary: "))
+                    if emp_salary < 40000:
+                        raise SalaryException
+                    emp_dept = input("What is the employee's department? ")
+                    emp_manager = False if input("Is employee manager? (yes/no) ") != "yes" else True
+                    new_empl = Employee(emp_first, emp_last, emp_salary, emp_dept, emp_manager)
+                    new_empl.gen_randomid()
+                    Department.add_employee(new_empl)         
+
+                except SalaryException:
+                        print("This salary is too low")
+    
+
+        elif viewCurrent == 3:
+            pass
         else:
             print("Please choose a valid option")
             
-        viewfinance = input("FINANCE DEPARTMENT\n1) View employees\n2) Add employees\n3) Return to previous menu.")
-        if viewfinance == 1:
-            print("Here are all the Employees {Employee.emplist}")
-        elif viewfinance == 2:
-            pass
-        elif viewfinance == 3:
-            pass
-        else:
-            print("Please choose a valid option")
+        # viewfinance = input("SALES DEPARTMENT\n1) View employees\n2) Add employees\n3) Return to previous menu.")
+        # if viewfinance == 1:
+        #     print("Here are all the Employees {Employee.emplist}")
+        # elif viewfinance == 2:
+        #     emp_input = input("New Employee input: ")
+        #     Department.add_employee(emp_input)
+        # elif viewfinance == 3:
+        #     pass
+        # else:
+        #     print("Please choose a valid option")
     elif start == 2:
         dept_input = input("New Department: ")
         new_dept = Department(dept_input)
